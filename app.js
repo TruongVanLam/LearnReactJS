d46e1c9ef787db57e946fa7ff4eb51e976ad17d8
TruongVanLam <truongvanlam@luvina.net>
2022-08-02 18:21:50 +0700

    commit

--- app.js
+++
@@ -1,6 +0,0 @@
-import {logger2} from './logger/index.js';
-import * as constants from './constants.js';
-
-
-// console.log(constants);
-logger2('Test massage...', constants.TYPE_ERROR);
--- logger/index.js
+++
@@ -1,1 +0,0 @@
-export {default as logger2} from './logger.js'
